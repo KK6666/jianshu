1267dce040aa18c45ee727912f5e43b4f42f671c
KK6666 <wwk66@outlook.com>
2019-08-13 15:18:43 +0800

    Updates

---
+++ precache-manifest.a602dc196427263df995fe414a39b420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7f2977ed4e8bd0f8081c221c67c0b5",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "f0a752f1dfd619e5e065",
+    "url": "/jianshu/static/js/2.4e93166e.chunk.js"
+  },
+  {
+    "revision": "7370b5f696355f7a8546",
+    "url": "/jianshu/static/js/3.cb862869.chunk.js"
+  },
+  {
+    "revision": "bd41933df93ba2c9f4a1",
+    "url": "/jianshu/static/js/main.49171b85.chunk.js"
+  },
+  {
+    "revision": "bee2e079c1a6435f08df",
+    "url": "/jianshu/static/js/runtime~main.aa679cd4.js"
+  }
+]);
