7fb629fb943b96afed1637e9de56bb81393449f9
KK6666 <wwk66@outlook.com>
2019-08-17 16:06:01 +0800

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,8 @@ import Article from './components/Article';
 import Recommend from './components/Recommend';
 import Download from './components/Download';
 import Writer from './components/Writer';
+import Slide from './components/Slide';
+import {Redirect} from "react-router-dom"
 import {
   HomeWrapper,
   HomeLeft,
@@ -15,12 +17,20 @@ import {connect} from 'react-redux';
 import {actionCreators} from './store';
 
 class Home extends PureComponent{
+  
   render(){
+    //当跳转至‘/jianshu？id=*'时，判断路由里的id是否存在，如果存在，说明是要跳转至对应detail/id
+    let id=this.getUrlParam('id')
+    if(id){
+      let url="/detail/"+id
+      return <Redirect to={url} />
+    }
+
     return(
       <HomeWrapper className='clearfix'>
         <HomeLeft>
-          <img className='pic' alt='' src='https://upload.jianshu.io/admin_banners/web_images/4592/22f5cfa984d47eaf3def6a48510cc87c157bf293.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540' />
-          <Topic />
+          <Slide />
+          {/* <Topic /> */}
           <Article />
         </HomeLeft>
         <HomeRight>
@@ -32,15 +42,26 @@ class Home extends PureComponent{
       </HomeWrapper>
     )
   }
-  
+
+  //获取当前路由里的参数值
+  getUrlParam(name){
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r!=null) return unescape(r[2]); return null;
+  }
+
+   
   componentDidMount(){
     this.props.changeHomeData();
     this.bindEvents();
+    //home页面挂载是将首页按钮的IndexActive设置为true
+    this.props.handleIndexActive(true);
   }
-
   //backtop组件卸载时，解除window上的事件绑定，避免此组件事件对其他组件造成影响
   componentWillUnmount(){
     window.removeEventListener('scroll',this.props.changeBackTop);
+    //home页面卸载是将首页按钮的IndexActive设置为false
+    this.props.handleIndexActive(false);
   }
 
   bindEvents(){
@@ -49,10 +70,20 @@ class Home extends PureComponent{
 
   //sscrollTo(xpos,ypos) 方法可把内容滚动到指定的坐标。
   handleBackTop(){
-    window.scrollTo(0,0);
+    // window.scrollTop(0,0); 无过渡动画
+    let scrollToTop = window.setInterval(function() {
+      // pageXOffset 和 pageYOffset 属性返回文档在窗口左上角水平和垂直方向滚动的像素。
+      let pos = window.pageYOffset;
+      if ( pos > 0 ) {
+          window.scrollTo( 0, pos - 50 ); // how far to scroll on each step
+      } else {
+          window.clearInterval( scrollToTop );
+      }
+    }, 2);
   }
-
 }
+
+
 
 const mapState=(state)=>({
   backTopShow:state.getIn(['home','backTopShow'])
@@ -72,7 +103,11 @@ const mapDispatch=(dispatch)=>({
       const action = actionCreators.getBackTop(false); 
       dispatch(action);
     }
-   
+  },
+  //首页按钮激活状态
+  handleIndexActive(boolean){
+    const action=actionCreators.indexActive(boolean);
+    dispatch(action);
   }
 })
 
