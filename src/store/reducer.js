d8c83bbe0d86b25b35d1313bb623a35bd437a836
KK6666 <wwk66@outlook.com>
2019-08-17 16:07:06 +0800

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,12 +4,14 @@ import {reducer as haederReducer} from '../common/header/store';
 import {reducer as homeReducer} from '../pages/home/store';
 import {reducer as detailReducer} from '../pages/detail/store'
 import {reducer as loginReducer} from '../pages/login/store'
+import {reducer as downloadReducer } from '../pages/downloadPage/store'
 
 const reducer = combineReducers({
   detail:detailReducer,
   header: haederReducer,
   home: homeReducer,
-  login: loginReducer
+  login: loginReducer,
+  download:downloadReducer
 });
 
 //导出的reducer为immutable对象，即index.js中使用的state
