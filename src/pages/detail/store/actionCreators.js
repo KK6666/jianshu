5ed37371a610be01648adfc5098ec230ac41b4e8
KK6666 <wwk66@outlook.com>
2019-08-17 16:03:53 +0800

    Update actionCreators.js

--- src/pages/detail/store/actionCreators.js
+++ src/pages/detail/store/actionCreators.js
@@ -1,5 +1,5 @@
 import * as constants from './constants';
-import Axios from 'axios';
+import axios from 'axios';
 
 export const getBackTop=(show)=>({
   type:constants.GET_BACK_TOP,
@@ -8,10 +8,12 @@ export const getBackTop=(show)=>({
 
 export const getDetailAction=(id)=>{
   return (dispatch)=>{
-    Axios.get('https://raw.githubusercontent.com/KK6666/jianshu/master/public/api/detail.json?id='+id)
+    // axios.get('/api/detail.json?id='+id)
+    
+    axios.get('https://raw.githubusercontent.com/KK6666/jianshu/dev/public/api/detail.json?id='+id)
       .then((res)=>{
-        const data = (res.data.data);
-
+        id--;
+        const data = res.data.data[id];
         const action={
           type:constants.GET_DETAIL,
           data:data
