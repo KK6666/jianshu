771ede77768d951350c6a32de536544953d1539c
KK6666 <wwk66@outlook.com>
2019-08-17 16:04:39 +0800

    Create loadable.js

---
+++ src/pages/downloadPage/loadable.js
@@ -0,0 +1,14 @@
+import Loadable from 'react-loadable';
+import React from 'react';
+import Loading from '../../common/components/loading.js'
+
+const LoadableComponent = Loadable({
+  // 异步加载，当前目录下index.js组件
+  loader: () => import('./'),
+  // 加载过程中，页面的临时显示组件
+  loading:Loading,
+  //加载时间不超过300ms不显示loading
+  delay:300
+});
+
+export default ()=><LoadableComponent/>
