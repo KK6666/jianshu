e7492161dfc831022e7b6d6b4a468445d0e4c8cf
KK6666 <wwk66@outlook.com>
2019-08-17 16:07:02 +0800

    Update style.js

--- src/pages/write/style.js
+++ src/pages/write/style.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper=styled.div`
-  height:500px;
-  width:500px;
-  background:pink;
+  width:100%;
+  text-align:center;
+  line-height:400px;
+  font-size:300%;
+
+
 `;
