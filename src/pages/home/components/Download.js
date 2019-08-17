10f91b9471992e17c7239976195bac3f936d4eab
KK6666 <wwk66@outlook.com>
2019-08-17 16:05:43 +0800

    Update Download.js

--- src/pages/home/components/Download.js
+++ src/pages/home/components/Download.js
@@ -1,16 +1,20 @@
 import React ,{PureComponent} from 'react';
 import {DownloadWrapper,DownloadRight} from '../style'
+import dImg from "../../../static/img/download.png"
+import {Link} from "react-router-dom"
 
 class Download extends PureComponent{
   render(){
     return(
-      <DownloadWrapper>
-        <img src='https://cdn2.jianshu.io/assets/web/download-index-side-qrcode-cb13fc9106a478795f8d10f9f632fccf.png' alt=''/>
-        <DownloadRight>
-          <div>下载简书手机App ></div>
-          <div>随时随地发现和创作内容</div>
-        </DownloadRight>
-      </DownloadWrapper>
+      <Link to="/downloadPage">
+        <DownloadWrapper>
+          <img src={dImg} alt=''/>
+          <DownloadRight>
+            <div>下载简书手机App ></div>
+            <div>随时随地发现和创作内容</div>
+          </DownloadRight>
+        </DownloadWrapper>
+      </Link>
     )
   }
 }
